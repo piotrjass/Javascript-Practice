afb69008d13746d35652291c66d0d39a50989b1d
Piotr <piotrjasiulewiczkontakt@gmail.com>
2024-03-21 22:48:31 +0100

    added 1 task

---
+++ 27.Task.js
@@ -0,0 +1,21 @@
+// url https://www.codewars.com/kata/54c9fcad28ec4c6e680011aa/train/javascript
+
+function isMerge(s, part1, part2) {
+  if (s.length !== part1.length + part2.length) {
+    return false;
+  }
+
+  if (!s.length) {
+    return true;
+  }
+
+  if (part1[0] === s[0] && isMerge(s.slice(1), part1.slice(1), part2)) {
+    return true;
+  }
+
+  if (part2[0] === s[0] && isMerge(s.slice(1), part1, part2.slice(1))) {
+    return true;
+  }
+
+  return false;
+}
